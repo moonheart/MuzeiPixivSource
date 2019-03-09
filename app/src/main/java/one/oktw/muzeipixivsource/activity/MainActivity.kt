5fab27dabb99fab4e3e637f94af684d35228a7b2
moonheart <fzp1205561571@hotmail.com>
2019-03-09 13:44:26 +0800

    使用协程加载图片

--- app/src/main/java/one/oktw/muzeipixivsource/activity/MainActivity.kt
+++ app/src/main/java/one/oktw/muzeipixivsource/activity/MainActivity.kt
@@ -1,31 +1,20 @@
 package one.oktw.muzeipixivsource.activity
 
-import android.content.ContentResolver
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
-import android.view.MotionEvent
-import android.widget.GridView
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
-import com.facebook.drawee.backends.pipeline.Fresco
-import com.facebook.imagepipeline.core.ImagePipelineConfig
 import com.google.android.apps.muzei.api.provider.Artwork
-import com.google.android.apps.muzei.api.provider.ProviderContract
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import one.oktw.muzeipixivsource.R
-
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.*
 import one.oktw.muzeipixivsource.adapter.IllustAdapter
-import one.oktw.muzeipixivsource.adapter.IllustAdapter3
 import one.oktw.muzeipixivsource.util.FileUtil
-import one.oktw.muzeipixivsource.util.GridItemDecoration
 import java.io.FileInputStream
-import kotlin.coroutines.CoroutineContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -52,21 +41,22 @@ class MainActivity : AppCompatActivity() {
             val cursor = contentResolver.query(Uri.parse("content://one.oktw.muzeipixivsource"), null, null, null, null)
             while (cursor.moveToNext()) {
                 if (cursor != null) {
-                    val uri = fileUtil.openFile(Artwork.fromCursor(cursor))
-                    val fis = FileInputStream(uri.path)
-                    val bitmap = BitmapFactory.decodeStream(fis)
-                    val imageInfo = IllustAdapter.ImageInfo(bitmap.height, bitmap.width, uri)
-                    val position = list.size
-                    list.add(imageInfo)
-                    mRecyclerView.post {
-                        illustAdapter.notifyItemRemoved(position)
-                        illustAdapter.notifyItemInserted(position)
+                    val artwork = Artwork.fromCursor(cursor)
+                    launch {
+                        val uri = fileUtil.openFile(artwork)
+                        val fis = FileInputStream(uri.path)
+                        val bitmap = BitmapFactory.decodeStream(fis)
+                        val imageInfo = IllustAdapter.ImageInfo(bitmap.height, bitmap.width, uri)
+                        val position = list.size
+                        list.add(imageInfo)
+                        mRecyclerView.post {
+                            illustAdapter.notifyItemRemoved(position)
+                            illustAdapter.notifyItemInserted(position)
+                        }
                     }
-
                 }
             }
         }
-
     }
 
 
