e8895079d8b2f433a7f6f4ffc2a3fe9db917dc6c
moonheart <fzp1205561571@hotmail.com>
2019-03-09 18:00:04 +0800

    长按菜单

--- app/src/main/java/one/oktw/muzeipixivsource/adapter/IllustAdapter.kt
+++ app/src/main/java/one/oktw/muzeipixivsource/adapter/IllustAdapter.kt
@@ -1,17 +1,21 @@
 package one.oktw.muzeipixivsource.adapter
 
 import android.content.Context
+import android.media.Image
 import android.net.Uri
+import android.view.ContextMenu
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.bumptech.glide.GenericTransitionOptions
 import com.bumptech.glide.Glide
 import com.google.android.apps.muzei.api.provider.Artwork
 import com.lxj.xpopup.XPopup
+import com.lxj.xpopup.interfaces.OnSelectListener
 import com.lxj.xpopup.interfaces.XPopupImageLoader
 import one.oktw.muzeipixivsource.R
 import one.oktw.muzeipixivsource.util.IllustUtil
@@ -52,11 +56,33 @@ class IllustAdapter(
         return position.toLong()
     }
 
-    inner class ViewHolder(val view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
+    inner class ViewHolder(
+        val view: View
+    ) : RecyclerView.ViewHolder(view), View.OnClickListener, View.OnCreateContextMenuListener, View.OnLongClickListener {
+        override fun onLongClick(v: View?): Boolean {
+            XPopup.get(context).asAttachList(arrayOf("不再显示此图片","不再显示此作品","分享此图片","分享作品链接"), null) { position, text ->
+                val imageInfo = imageInfos[adapterPosition] as ImageInfo
+                Toast.makeText(context, "$position, $text", Toast.LENGTH_SHORT).show()
+                when(position){
+                    0-> illustUtil.hideImage(imageInfo.artwork)
+                    1-> illustUtil.hideIllust(imageInfo.artwork)
+                    2-> illustUtil.shareImage(imageInfo.artwork)
+                    3-> illustUtil.shareUrl(imageInfo.artwork)
+                }
+            }.show()
+            return true
+        }
 
         var imageView: ImageView = view.findViewById(R.id.my_image_view);
 
+        override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+
+        }
+
         init {
+            XPopup.get(context).watch(view)
+            view.setOnLongClickListener(this)
+            view.setOnCreateContextMenuListener(this)
             view.setOnClickListener(this)
         }
 
